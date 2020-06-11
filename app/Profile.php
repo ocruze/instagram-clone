4c7ce37370816b3fd3ba577f2bb899e68d0e4caf
Orka Arnest CRUZE <o.cruze@live.com>
2020-06-12 00:42:07 +0200

    profile: default dp

--- app/Profile.php
+++ app/Profile.php
@@ -12,4 +12,10 @@ class Profile extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function profileImage()
+    {
+        $imagePath = $this->image ? $this->image : 'profile/default.jpg';
+        return '/storage/' . $imagePath;
+    }
 }
